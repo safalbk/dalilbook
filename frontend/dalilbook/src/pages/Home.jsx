1091060d0d1ae3c7a6e42d7d3d6b1882152fece2
vinu-viswanath <vinuviswanath6@gmail.com>
2025-11-01 00:35:55 +0530

    cards in component

--- frontend/dalilbook/src/pages/Home.jsx
+++ frontend/dalilbook/src/pages/Home.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import DescriptionCard from "../components/DescriptionCard/DescriptionCard";
+import FileUpload from "../components/FileUploadCard/FileUpload";
+import ImageCard from "../components/ImageCard/ImageCard";
+import NotesCard from "../components/NotesCard/NotesCard";
+import TextCard from "../components/TextCard/TextCard";
+import TopicsCard from "../components/TopicsCard/TopicsCard";
+import VideoCard from "../components/VideoCard/VideoCard";
 
 function Home() {
   return (
-    <div className='bg-red-400'>Home Page
-    
+    <div className="bg-red-400 pt-16">
+      Home Page
+      <DescriptionCard />
+      <FileUpload />
+      <ImageCard />
+      <NotesCard />
+      <TextCard />
+      <TopicsCard />
+      <VideoCard/>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
