5ad27b83438a4d09a11fb0cb875bd8180e7f97f1
safalbk <safalbk333@gmail.com>
2025-11-03 17:56:01 +0530

    backend added

---
+++ backend/dalilbook/src/main/java/com/arif/dalilbook/DalilbookApplication.java
@@ -0,0 +1,13 @@
+package com.arif.dalilbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalilbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalilbookApplication.class, args);
+	}
+
+}
---
+++ backend/dalilbook/src/test/java/com/arif/dalilbook/DalilbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arif.dalilbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalilbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
